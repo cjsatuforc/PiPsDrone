759b9f92e9e3154716bf2bfde0ab59398e4f93fa
Irwin <irwinwilliams@users.noreply.github.com>
2015-09-16 16:43:37 -0400

    Update index2.js
    Added code to watch pi responses

--- index2.js
+++ index2.js
@@ -1,20 +1,43 @@
-var dualShock  = require('dualshock-controller');
+///Dualshock code started from:
+///https://github.com/rdepena/node-dualshock-controller
+var dualShock = require('dualshock-controller');
+
+//pass options to init the controller.
 var controller = dualShock(
+    {
+        //you can use a ds4 by uncommenting this line.
+        //config: "dualshock4-generic-driver",
+        //if using ds4 comment this line.
+        config : "dualShock3",
+        //smooths the output from the acelerometers (moving averages) defaults to true
+        accelerometerSmoothing : true,
+        //smooths the output from the analog sticks (moving averages) defaults to false
+        analogStickSmoothing : false
+    });
+
+function s(msg)
 {
-config:"dualShock3",
-accelerometerSmoothing: true,
-analogStickSmoothing: false
+	console.log(msg);
+}
+
+//make sure you add an error event handler
+controller.on('error', function(data) {
+	s("ERROR: "+data);
 });
 
-controller.on('error', function(data)
-{
-
-	console.log("ERR");
-	console.log(data);
+//add event handlers:
+controller.on('left:move', function(data) {
+  s("left: "+data);
 });
-
-controller.on('left:move', function(data)
-{
-	console.log("left");
-	console.log(data);
+controller.on('right:move', function(data) {
+  s("right: "+data);
+});
+controller.on('connected', function(data) {
+  s("connected: "+data);
+});
+controller.on('square:press', function (data) {
+  s("square press: "+data);
+});
+controller.on('square:release', function (data) {
+  s("square release: "+data);
 });
