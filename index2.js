93d57d00d69b6e57e10a7cf9cf051a2f33333697
Irwin <irwinwilliams@users.noreply.github.com>
2015-09-16 17:14:43 -0400

    Update index2.js
    Added code for ar-drone control.

--- index2.js
+++ index2.js
@@ -1,43 +1,96 @@
 ///Dualshock code started from:
 ///https://github.com/rdepena/node-dualshock-controller
 var dualShock = require('dualshock-controller');
+///most of the drone came from Anderson 'Aqua Lad' Williams - Interned at Teleios for July-Aug, 2015.
+///big up, Andy!
+var arDrone = require('ar-drone');
 
-//pass options to init the controller.
 var controller = dualShock(
     {
-        //you can use a ds4 by uncommenting this line.
-        //config: "dualshock4-generic-driver",
-        //if using ds4 comment this line.
-        config : "dualShock3",
-        //smooths the output from the acelerometers (moving averages) defaults to true
-        accelerometerSmoothing : true,
-        //smooths the output from the analog sticks (moving averages) defaults to false
-        analogStickSmoothing : false
+        config: "dualShock3",
+        accelerometerSmoothing: true,
+        analogStickSmoothing: false
     });
 
-function s(msg)
-{
-	console.log(msg);
+var client = arDrone.createClient();
+
+function s(msg) {
+    console.log(msg);
 }
 
 //make sure you add an error event handler
-controller.on('error', function(data) {
-	s("ERROR: "+data);
+controller.on('error', function (data) {
+    s("ERROR: " + data);
 });
 
 //add event handlers:
-controller.on('left:move', function(data) {
-  s("left: "+data);
+controller.on('left:move', function (data) {
+    s("left: " + data);
+    client.left(0.5);
 });
-controller.on('right:move', function(data) {
-  s("right: "+data);
+controller.on('right:move', function (data) {
+    s("right: " + data);
+    client.left(0.5);
 });
-controller.on('connected', function(data) {
-  s("connected: "+data);
+controller.on('connected', function (data) {
+    s("connected: " + data);
 });
 controller.on('square:press', function (data) {
-  s("square press: "+data);
+    s("square press: " + data);
+    client.animateLeds('blinkGreen', 5, 2);
+    client.takeoff(function () {
+        hovering = true;
+        console.log("flying");
+        this.calibrate(0);
+    });
 });
-controller.on('square:release', function (data) {
-  s("square release: "+data);
+
+
+
+controller.on('triangle:press', function (data) {
+    s("triangle press: " + data);
+    client.front(0.5);
+});
+
+controller.on('dpadUp:press', function (data) {
+    s("dpadUp press: " + data);
+    client.up(0.5);    
+});
+controller.on('dpadRight:press', function (data) {
+    s("dpadRight press: " + data);
+    client.counterClockwise(0.5);
+});
+controller.on('dpadDown:press', function (data) {
+    s("dpadDown press: " + data);
+    client.down(0.5);
+});
+controller.on('dpadLeft:press', function (data) {
+    s("dpadLeft press: " + data);
+    client.clockwise(0.5);
+});
+
+controller.on('psxButton:press', function (data) {
+    s("psxButton press: " + data);
+    var animOptions = ['phiM30Deg', 'phi30Deg', 'thetaM30Deg', 'theta30Deg', 'theta20degYaw200deg',
+       'theta20degYawM200deg', 'turnaround', 'turnaroundGodown', 'yawShake',
+       'yawDance', 'phiDance', 'thetaDance', 'vzDance', 'wave', 'phiThetaMixed',
+       'doublePhiThetaMixed', 'flipAhead', 'flipBehind', 'flipLeft', 'flipRight'];
+    var min = 0;
+    var max = animOptions.length - 1;
+    var idx = Math.floor(Math.random() * (max - min)) + min;
+    client.animate(animOptions[idx], 3000);
+
+    client.animateLeds('blinkGreen', 5, 2);
+});
+
+controller.on('start:press', function (data) {
+    s("start press: " + data);
+    client.calibrate(0);
+    client.stop();
+    
+});
+
+controller.on('select:press', function (data) {
+    s("start press: " + data);
+    client.land();
 });
